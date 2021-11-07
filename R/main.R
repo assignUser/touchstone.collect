12185d0424b8ed945994987a7eda3ace880449a2
assignUser <jacob@wujciak.de>
2021-11-07 14:36:45 +0100

    adjust timings

--- R/main.R
+++ R/main.R
@@ -2,23 +2,23 @@
 # n ~ 5000 = 2s
 # n ~ 10000 = 8s
 fillMatrix <- function(n) {
-  matrix <- matrix(0, n, n)
-  for (i in 1:n) {
-    for (j in 1:n) {
-      matrix[i, j] <- i*j
+    matrix <- matrix(0, n, n)
+    for (i in 1:n) {
+        for (j in 1:n) {
+            matrix[i, j] <- i * j
+        }
     }
-  }
-  return(matrix)
+    return(matrix)
 }
 
 base_faster <- function(n) {
-  fillMatrix(n)
+    Sys.getenv("TC_DELAY") %>%
+        as.numerc() %>%
+        Sys.sleep()
+    fillMatrix(n)
 }
 
 #' @importFrom magrittr %>%
 head_faster <- function(n) {
-  Sys.getenv("TC_DELAY") %>%
-    as.numerc() %>%
-    Sys.sleep()
-  fillMatrix(n)
+    fillMatrix(n)
 }
